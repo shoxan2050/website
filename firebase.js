9972b627faa49cd0bdfd8198b93c762d83308244
shoxan2050 <boburjonbaratov97@gmail.com>
2026-01-07 10:17:19 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,21 +1,22 @@
-// 1. Firebase core
-import { initializeApp } from "firebase/app";
+// firebase.js  (BROWSER UCHUN TO‘G‘RI VARIANT)
 
-// 2. Firebase AUTH
-import { getAuth } from "firebase/auth";
+// Firebase SDK (CDN)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
 
-// 3. Config (seniki)
+// Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyCy8SLCFTgc_mB8B4JJqrUGsX5Lr2NUuPo",
   authDomain: "space0411-a00b1.firebaseapp.com",
   projectId: "space0411-a00b1",
   storageBucket: "space0411-a00b1.firebasestorage.app",
   messagingSenderId: "1018550863390",
-  appId: "1:1018550863390:web:d023ce44fbe8ce7171d00f",
+  appId: "1:1018550863390:web:d023ce44fbe8ce7171d00f"
 };
 
-// 4. App
+// Initialize
 const app = initializeApp(firebaseConfig);
-
-// 5. AUTH ISHGA TUSHDI 🔐
 const auth = getAuth(app);
+
+// EXPORT (MUHIM!)
+export { auth };
