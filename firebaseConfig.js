e10b97e2300f1bf913180d9fa41831c3da1c3c43
shoxan2050 <boburjonbaratov97@gmail.com>
2026-01-07 11:38:54 +0500

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCy8SLCFTgc_mB8B4JJqrUGsX5Lr2NUuPo",
+  authDomain: "space0411-a00b1.firebaseapp.com",
+  databaseURL: "https://space0411-a00b1-default-rtdb.firebaseio.com",
+  projectId: "space0411-a00b1",
+  storageBucket: "space0411-a00b1.appspot.com",
+  messagingSenderId: "1018550863390",
+  appId: "1:1018550863390:web:d023ce44fbe8ce7171d00f"
+};
