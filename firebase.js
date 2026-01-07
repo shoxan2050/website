792efe785ac70f0bb9b8a601fbef7ce945e2185c
shoxan2050 <boburjonbaratov97@gmail.com>
2026-01-07 22:38:35 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,22 +1,20 @@
-// firebase.js
+// firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB-Bp_gjuChFhdGxRTPIb5w4zGS025ITx4",
-  authDomain: "smarter-a99f4.firebaseapp.com",
-
-  // 🔥 MANA ENG MUHIM QATOR
-  databaseURL: "https://smarter-a99f4-default-rtdb.firebaseio.com",
-
-  projectId: "smarter-a99f4",
-  storageBucket: "smarter-a99f4.firebasestorage.app",
-  messagingSenderId: "201127037803",
-  appId: "1:201127037803:web:32f85148679a0314645a2e"
+  apiKey: "AIzaSyCy8SLCFTgc_mB8B4JJqrUGsX5Lr2NUuPo",
+  authDomain: "space0411-a00b1.firebaseapp.com",
+  databaseURL: "https://space0411-a00b1-default-rtdb.firebaseio.com",
+  projectId: "space0411-a00b1",
+  storageBucket: "space0411-a00b1.firebasestorage.app",
+  messagingSenderId: "1018550863390",
+  appId: "1:1018550863390:web:d023ce44fbe8ce7171d00f"
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-export const db = getDatabase(app);
+export const database = getDatabase(app);
+export { onAuthStateChanged };
